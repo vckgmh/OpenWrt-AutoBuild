74496d6d3ecfd3f52ab46f87f03a77770f28a932
vckgmh <vckgmh@gmail.com>
2022-07-17 11:25:46 +0800

    update

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -251,6 +251,9 @@ CONFIG_PACKAGE_luci-app-dockerman=y
 # kodexplorer可道云
 CONFIG_PACKAGE_luci-app-kodexplorer=y
 
+# 阿里云盘webdav
+CONFIG_PACKAGE_luci-app-aliyundrive-webdav
+
 EOF
 
 sed -i 's/^[ \t]*//g' ./.config
