2214e3d3a82b296ae2fa360580afb9f98ebaf119
vckgmh <vckgmh@gmail.com>
2022-11-07 16:17:05 +0800

    update

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -251,10 +251,8 @@ CONFIG_PACKAGE_luci-app-dockerman=y
 # kodexplorer可道云
 CONFIG_PACKAGE_luci-app-kodexplorer=y
 
-# 阿里云盘webdav
-CONFIG_PACKAGE_luci-app-aliyundrive-webdav=y
-
-CONFIG_PACKAGE_luci-app-aliyundrive-fuse=y
+# ZeroTier内网穿透
+CONFIG_PACKAGE_luci-app-zerotier=y
 
 EOF
 
